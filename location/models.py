0b1afe6e6520fb4a6daddde32fe42ba033c6d5bc
bekaneo <vealtto@gmail.com>
2022-10-03 22:00:43 +0600

    Locations Models, Views and URLs

--- location/models.py
+++ location/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+DIFFICULTY = (
+    (1, 'Easy'),
+    (2, 'Medium'),
+    (3, 'Hard')
+)
+
+
+class Region(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    image = models.ImageField(upload_to='region')
+    image_map = models.ImageField(upload_to='region')
+
+
+class Leisure(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, primary_key=True)
+
+
+class Location(models.Model):
+    title = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='location')
+    description = models.TextField()
+    leisure = models.ForeignKey(Leisure, on_delete=models.CASCADE, related_name='location')
+    difficulty = models.CharField(choices=DIFFICULTY, max_length=10)
+
+
+class Image(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='location')
--- location/views.py
+++ location/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from location.models import Location, Region, Leisure
+
+
+class ListLocationView(ListView):
+    def get(self, request, *args, **kwargs):
+        locations = Location.objects.all()
+        context = {
+            'locations': locations
+        }
+        return render(request, '#', context)
+
+
+class DetailLocationView(DetailView):
+    def get(self, request, pk, *args, **kwargs):
+        location = Location.objects.get(id=pk)
+        context = {
+            'location': location
+        }
+
+        return render(request, '#', context)
+
+
+class DetailRegionView(DetailView):
+    def get(self, request, slug, *args, **kwargs):
+        region = Region.objects.get(slug=slug)
+        locations = Location.objects.filter(region=region)
+        context = {
+            'region': region,
+            'location': locations
+        }
+
+        return render(request, '#', context)
+
+
+class DetailLeisureView(DetailView):
+    def get(self, request, slug, *args, **kwargs):
+        leisure = Leisure.objects.get(slug=slug)
+        locations = Location.objects.filter(leisure=leisure)
+        context = {
+            'leisure': leisure,
+            'location': locations
+        }
+
+        return render(request, '#', context)
+
