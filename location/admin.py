e876a683d0c09ab7a6f94eb96828cc460e548a18
Avtandil Kurbanov <avtandilkgg@gmail.com>
2022-10-05 12:07:23 +0600

    qwe

--- location/admin.py
+++ location/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Location, Region, Leisure
+from .models import Location, Region, Leisure, Image
+
+
 # Register your models here.
 
-
+admin.site.register(Image)
 admin.site.register(Location)
 admin.site.register(Region)
 admin.site.register(Leisure)
--- location/views.py
+++ location/views.py
@@ -1,47 +1,77 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from location.models import Location, Region, Leisure
+from location.models import Location, Region, Leisure, Image
+
+
+class ListPostView(ListView):
+    def get(self, request, *args, **kwargs):
+        locations = Location.objects.all()
+        regions = Region.objects.all()
+        context = {
+            'posts': locations,
+            'regions': regions,
+        }
+        return render(request, 'location/all_posts.html', context)
 
 
 class ListLocationView(ListView):
     def get(self, request, *args, **kwargs):
         locations = Location.objects.all()
+        data = {}
+        for items in locations:
+            data['location'] = items
+            data['image'] = Image.objects.filter(location=items)
+        regions = Region.objects.all()
         context = {
-            'locations': locations
+            'locations': data,
+            'regions': regions,
+
         }
-        return render(request, '#', context)
+        return render(request, 'location/index.html', context)
 
 
 class DetailLocationView(DetailView):
     def get(self, request, pk, *args, **kwargs):
         location = Location.objects.get(id=pk)
+        regions = Region.objects.all()
+        region = Region.objects.get(location=location)
+        leisure = Leisure.objects.get(location=location)
+        images = Image.objects.filter(location=location)
         context = {
-            'location': location
+            'location': location,
+            'regions': regions,
+            'region': region,
+            'leisure': leisure,
+            'images': images
         }
 
-        return render(request, '#', context)
+        return render(request, 'location/detail.html', context)
 
 
 class DetailRegionView(DetailView):
     def get(self, request, slug, *args, **kwargs):
+        regions = Region.objects.all()
         region = Region.objects.get(slug=slug)
         locations = Location.objects.filter(region=region)
         context = {
             'region': region,
+            'regions': regions,
             'location': locations
         }
 
-        return render(request, '#', context)
+        return render(request, 'location/detail_region.html', context)
 
 
 class DetailLeisureView(DetailView):
     def get(self, request, slug, *args, **kwargs):
         leisure = Leisure.objects.get(slug=slug)
         locations = Location.objects.filter(leisure=leisure)
+        regions = Region.objects.all()
         context = {
             'leisure': leisure,
-            'location': locations
+            'locations': locations,
+            'regions': regions
         }
 
-        return render(request, '#', context)
+        return render(request, 'location/lesure_detail.html', context)
