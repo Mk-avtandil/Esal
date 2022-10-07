894f537b642d02cf2e7fe958129f4c5ab7ba4dde
bekaneo <vealtto@gmail.com>
2022-10-07 13:33:52 +0600

    fix

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login', views.LoginPageView.as_view(), name="login"),
     # path('register', views.register_page, name="register"),
     path('logout', views.logout_page, name="logout"),
-    path('home', views.dummy_page, name="dummy_index"),
+    # path('home', views.dummy_page, name="dummy_index"),
     path('register', views.RegisterPage.as_view(), name="register"),
 ]
--- account/views.py
+++ account/views.py
@@ -7,27 +7,10 @@ from django.views.generic import CreateView, View
 from django.urls import reverse_lazy
 
 
-# Create your views here.
-
-
 class RegisterPage(CreateView):
     form_class = CreateUserForm
     success_url = reverse_lazy('login')
     template_name = "account/register.html"
-
-
-# def register_page(request):
-#     form = CreateUserForm()
-#     regions = Region.objects.all()
-#     if request.method == "POST":
-#         form = CreateUserForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             user = form.cleaned_data.get('email')
-#             messages.success(request, "An account was successfully created for " + user)
-#             return redirect('login')
-#     context = {'form': form, 'regions': regions}
-#     return render(request, 'account/register.html', context)
 
 
 class LoginPageView(View):
@@ -45,7 +28,7 @@ class LoginPageView(View):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('posts')
+            return redirect('/')
         message = "Login Failed. Username or password is incorrect"
         regions = Region.objects.all()
         context = {
@@ -55,37 +38,6 @@ class LoginPageView(View):
         return render(request, "account/login.html", context)
 
 
-# def login_page(request):
-#     regions = Region.objects.all()
-#     if request.user.is_authenticated:
-#         return redirect('posts')
-#     else:
-#         if request.method == 'POST':
-#             username = request.POST.get('username')
-#             password = request.POST.get('password')
-#             print(username)
-#             print(password)
-#
-#             user = authenticate(request, username=username, password=password)
-#             print(user)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('posts')
-#             else:
-#                 messages.info(request, 'Username OR password is incorrect')
-#         context = {'regions': regions}
-#         return render(request, 'account/login.html', context)
-
-
 def logout_page(request):
     logout(request)
     return redirect('posts')
-
-
-# def dummy_page(request):
-#     return render(request, "account/dummy_index.html")
-#
-#
-# def index(request):
-#     context = {}
-#     return render(request, "location/index.html", context)
