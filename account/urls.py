494932f526f5f7d8b273ad0662d88bb39cb9559f
praktikant <ek27global312@gmail.com>
2022-10-07 11:36:10 +0600

    Final: User class-based views

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login_page, name="login"),
-    path('register', views.register_page, name="register"),
+    path('login', views.LoginPageView.as_view(), name="login"),
+    # path('register', views.register_page, name="register"),
     path('logout', views.logout_page, name="logout"),
     path('home', views.dummy_page, name="dummy_index"),
+    path('register', views.RegisterPage.as_view(), name="register"),
 ]
